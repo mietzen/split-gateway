0b5bfbf276d34c68ef1916b2862c9c8be3aa5fdc
Nils Stein <social.nst@mailbox.org>
2022-07-30 19:16:48 +0200

    fixed install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-    then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
     exit 1
 fi
 
@@ -11,16 +11,19 @@ cp ./config /etc/split-gateway/config
 if [[ -z "${EXTERNAL_INTERFACE}" ]]; then 
     EXTERNAL_INTERFACE=$(ip route | tail -2 | head -1 | grep -oP '(?<=dev )eth[[:digit:]]{1,3}\.*[[:digit:]]{0,5}')
     echo "External interface not set, guessed interface: ${EXTERNAL_INTERFACE}"
+fi
 echo "EXTERNAL_INTERFACE='${EXTERNAL_INTERFACE}'" >> /etc/split-gateway/config
 
 if [[ -z "${EXTERNAL_GATEWAY}" ]]; then 
     EXTERNAL_GATEWAY=$(ip route | grep ${EXTERNAL_GATEWAY} | cut -d' ' -f1 | cut -d'/' -f1 | sed 's/.$/1/')
     echo "External gateway not set, guessed gateway: ${EXTERNAL_GATEWAY}"
+fi
 echo "EXTERNAL_GATEWAY='${EXTERNAL_GATEWAY}'" >> /etc/split-gateway/config
 
 if [[ -z "${NETWORK}" ]]; then 
     NETWORK=$(ip route | grep ${EXTERNAL_INTERFACE} | cut -d' ' -f1)
     echo "Network not set, guessed network: ${NETWORK}"
+fi
 echo "NETWORK='${NETWORK}'" >> /etc/split-gateway/config
 
 cp ./init-split-gateway.sh /etc/split-gateway/init-split-gateway.sh
